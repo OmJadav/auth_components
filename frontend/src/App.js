bc5425d3aa84806e4ec4173dc955af3bce8ba992
Om Jadav <omjadav04@gmail.com>
2024-05-15 22:31:32 +0530

    customization of image completed

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Loginpage } from './components/Loginpage';
+
+function App() {
+  return (
+    <Loginpage />
+  );
+}
+
+export default App;
